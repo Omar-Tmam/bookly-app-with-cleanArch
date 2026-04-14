2085828134b9be5084148681c86f29307c381af7
omartmam <omartmam9@gmail.com>
2026-04-14 18:09:20 +0200

    finish home repo

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,3 +1,6 @@
+import 'package:bookly_clean_arch/Features/home/domain/entities/book_entity.dart';
+
 abstract class HomeRepo {
-  
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
