66f042348f5eae9c4c519ed7a30b3cbcd440c91f
omartmam <omartmam9@gmail.com>
2026-04-14 18:26:53 +0200

    create featuredbooks use case

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,7 @@
+import 'package:bookly_clean_arch/Features/home/domain/repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+}
