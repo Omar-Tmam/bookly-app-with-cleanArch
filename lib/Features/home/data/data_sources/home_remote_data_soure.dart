2face29267741d6c2c9b56513cd14bd4b1556521
omartmam <omartmam9@gmail.com>
2026-04-16 17:31:37 +0200

    refactor HomeRemoteDataSource implementation for improved readability and consistency

--- lib/Features/home/data/data_sources/home_remote_data_soure.dart
+++ lib/Features/home/data/data_sources/home_remote_data_soure.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_clean_arch/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_clean_arch/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/core/utils/api_service.dart';
-import 'package:dio/dio.dart';
 
 abstract class HomeRemoteDataSoure {
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -11,24 +10,30 @@ abstract class HomeRemoteDataSoure {
 class HomeRemoteDataSoureImpl extends HomeRemoteDataSoure {
   final ApiService apiService;
   HomeRemoteDataSoureImpl(this.apiService);
+
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() async{
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
     List<BookEntity> books = [];
-    
-   var data = await apiService.get(endPoint: 'volumes?filter=free-ebooks&q=programming');
-   getBooksList(data, books);
-   return books;
-  }
 
-  void getBooksList(Map<String, dynamic> data, List<BookEntity> books) {
-    for(var item in data['items']){
-     books.add(BookModel.fromJson(item));
-    }
+    var data = await apiService.get(endPoint: 'volumes?filter=free-ebooks&q=programming');
+    getBooksList(data, books);
+    return books;
   }
 
   @override
-  Future<List<BookEntity>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    List<BookEntity> books = [];
+
+    var data = await apiService.get(
+      endPoint: 'volumes?filter=free-ebooks&q=programming&sorting=newest',
+    );
+    getBooksList(data, books);
+    return books;
+  }
+}
+
+void getBooksList(Map<String, dynamic> data, List<BookEntity> books) {
+  for (var item in data['items']) {
+    books.add(BookModel.fromJson(item));
   }
 }
