6ba7e04d287a5b74ee13d6a1bbcca216b043d2a6
omartmam <omartmam9@gmail.com>
2026-04-17 15:41:57 +0200

    refactor HomeLocalDataSourceImpl to implement fetchFeaturedBooks and fetchNewestBooks methods

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -1,19 +1,22 @@
 import 'package:bookly_clean_arch/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_clean_arch/constants.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks();
   List<BookEntity> fetchNewestBooks();
 }
-class HomeLocalDataSourceImpl extends HomeLocalDataSource{
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
   List<BookEntity> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kNewestBox);
+    return box.values.toList();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:bookly_clean_arch/constants.dart';
 import 'package:bookly_clean_arch/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main()async {
+void main() async {
+   await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
   runApp(const Bookly());
-  Hive.registerAdapter( BookEntityAdapter());
- await Hive.openBox(kFeaturedBox);
+
 }
 
 class Bookly extends StatelessWidget {
