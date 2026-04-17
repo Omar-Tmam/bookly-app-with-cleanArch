c957307d4ebd4eb794a1351dffd57b07341b8df6
omartmam <omartmam9@gmail.com>
2026-04-17 15:12:02 +0200

    implement BookEntity and its TypeAdapter for Hive storage

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,12 +1,29 @@
+import 'package:hive/hive.dart';
+
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
+
+
 class BookEntity {
+  @HiveField(0)
   final String? bookId;
+  @HiveField(1)
   final String? img;
+  @HiveField(2)
   final String? title;
+  @HiveField(3)
   final String? author;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final num? rating;
 
-  BookEntity({required this.bookId, required this.img, required this.title, required this.author, required this.price, required this.rating});
-
-  
+  BookEntity({
+    required this.bookId,
+    required this.img,
+    required this.title,
+    required this.author,
+    required this.price,
+    required this.rating,
+  });
 }
